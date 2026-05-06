7e2e2ee57bd38d4675f9cc951c754aaebccffcd6
Ronny Bergmann <git@ronnybergmann.net>
2026-05-06 19:17:44 +0200

    a first working version of geodesics.

--- src/geodesics.jl
+++ src/geodesics.jl
@@ -9,6 +9,8 @@ Creates a connected geodesic plot connecting all or `points` sequentially.
     Makie.documented_attributes(Lines)...
     "Set the number of samples used per geodesic"
     samples = 100
+    "Set the curve to be closed by connecting the last and first point as well"
+    closed = false
 end
 
 function Makie.plot!(p::Geodesics{<:Tuple{AM, V}}) where {AM <: Manifolds.AbstractManifold, V}
@@ -18,12 +20,17 @@ function Makie.plot!(p::Geodesics{<:Tuple{AM, V}}) where {AM <: Manifolds.Abstra
     points = p[:points][]
     n = length(points)
     s = p[:samples][]
-    @info "s = " s
     for i in 1:(n - 1)
         p1 = points[i]
-        p2 = points[i]
-        pts = shortest_geodesic(M, p1, p2, range(; start = 0.0, stop = 1.0, length = n))
-        lines!(p, p.attributes, M, pts)
+        p2 = points[i + 1]
+        pts = shortest_geodesic(M, p1, p2, range(; start = 0.0, stop = 1.0, length = s))
+        lines!(p, p.attributes, M, Point3f.(pts))
+    end
+    if p[:closed][]
+        p1 = points[end]
+        p2 = points[1]
+        pts = shortest_geodesic(M, p1, p2, range(; start = 0.0, stop = 1.0, length = s))
+        lines!(p, p.attributes, M, Point3f.(pts))
     end
     return p
 end
