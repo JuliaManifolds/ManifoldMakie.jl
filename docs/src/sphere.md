de4d066a8681dcaa937271d1a64259bca844257f
Ronny Bergmann <git@ronnybergmann.net>
2026-05-21 19:37:12 +0200

    Add S1 code and example

--- docs/src/sphere.md
+++ docs/src/sphere.md
@@ -1,4 +1,34 @@
-# Plotting data on the 2D Sphere
+# Plotting data on Spheres
+
+On a sphere we can plot points and tangent vectors with the default
+[`scatter`](@extref `Makie.scatter`) and [`arrows3d`](@extref `Makie.arrows3d`).
+To start a plot on a manifold, there is usually a `manifoldplot` function, here [`sphereplot`](@ref)
+to set up the figure and add here for example the sphere the data lives on.
+
+## The 1D Sphere ``𝕊^1``
+
+For the one-dimensional sphere, we obtain unit vectors in ``ℝ^2`` or in other words points on the circle.
+
+```@example
+using Manifolds, ManifoldMakie, GLMakie
+
+M = Manifolds.Sphere(1)
+# Since S2 is the default case and its color is white, set S1 surface/boundary color
+fig, ax, pl = sphereplot(M; surfacecolor=:gray)
+
+p = [1.0, 0.0]
+q = [1/sqrt(2), -1/sqrt(2)]
+r = [1/sqrt(2), 1/sqrt(2)]
+pts = shortest_geodesic(M, p, q, 0:0.05:1.0)
+vecs = [log(M, s, r) for s in pts]
+
+arrows2d!(ax, M, pts, vecs; color = :blue)
+scatter!(ax, M, pts; color = :green, markersize = 16)
+scatter!(ax, M, [r,]; color = :orange, markersize = 16)
+fig
+```
+
+## The 2D Sphere ``𝕊^2``
 
 On a sphere we can plot points and tangent vectors with the default
 [`scatter`](@extref `Makie.scatter`) and [`arrows3d`](@extref `Makie.arrows3d`).
@@ -17,12 +47,12 @@ r = [1/sqrt(2), 0.0, 1/sqrt(2)]
 pts = shortest_geodesic(M, p, q, 0:0.05:1.0)
 vecs = [log(M, s, r) for s in pts]
 
-scatter!(ax, M, pts; color = :green, markersize = 16)
-scatter!(ax, M, [r,]; color = :orange, markersize = 16)
 arrows3d!(
     ax, M, pts, vecs; color = :blue,
     minshaftlength = 0, shaftlength=.99, shaftradius = 0.004, tipradius = 0.016, tiplength = 0.1,
 )
+scatter!(ax, M, pts; color = :green, markersize = 16)
+scatter!(ax, M, [r,]; color = :orange, markersize = 16)
 fig
 ```
 
--- src/sphere.jl
+++ src/sphere.jl
@@ -1,29 +1,68 @@
 """
-    sphereplot(::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{2}}}; kwargs...)
+    sphereplot(::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{n}}}; kwargs...)
 
-Draw the [`Sphere`](@extref `Manifolds.Sphere`)`(2)` as a (transparent) surface with an overlaid wireframe.
+Draw the [`Sphere`](@extref `Manifolds.Sphere`)`(n)`, ``n=1,2`` as a (transparent) surface with an overlaid wireframe.
 This can be combined with
 * [`scatter`](@extref `Makie.scatter`)`(M, pts)` to plot points thereon
 * [`arrows3d`](@extref `Makie.arrows3d`)`(M, pts, vecs)` to plot tangent vectors
 * [`geodesics`](@ref)`(M, pst)` and [`scattergeodesics`](@ref)`(M, pst)` to draw geodesics
 
+## Examples
+
 ```julia
 fig, ax, p = sphereplot(Manifolds.Sphere(2))
 ```
+
+for the 2-sphere ``𝕊^2`` embedded in ``ℝ^3``
+
+
+ ```julia
+fig, ax, p = sphereplot(Manifolds.Sphere(1))
+```
+
+for the cyclic data on ``𝕊^1`` embedded in ``ℝ^2``
 """
 @recipe SpherePlot (M,) begin
-    "Color of the wireframe lines drawn on top of the surface."
+    "Color of the wireframe lines drawn on top of the surface (``𝕊^2``)."
     wirecolor = (:lightsteelblue, 0.4)
-    "Tessellation resolution (segments of the wireframe in every direction)."
+    "Tessellation resolution (segments of the wireframe in every direction, ``𝕊^2``)."
     wires = 24
-    "linewidth of the wire"
+    "linewidth of the wire (``𝕊^2``)"
     wirewidth = 0.5
-    "Solid color of the surface."
-    surfacecolor = :white
+    "Solid color of the surface (``𝕊^2``) or boundary (``𝕊^1``)."
+    surfacecolor = :gray
     "Surface alpha (0 = transparent, 1 = opaque)."
-    surfacealpha = 0.5
+    surfacealpha = 0.33
+    "thickness of the boundary surface (only ``𝕊^1``)"
+    surfaceboundary = 2
     # add the other default plot attributes here as well
     Makie.mixin_generic_plot_attributes()...
+end
+
+#
+#
+# 𝕊^1
+function Makie.plot!(p::SpherePlot{<:Tuple{Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{1}}}}})
+    map!(p.attributes, [:M], :circle) do M
+        return GeometryBasics.Circle(Point2f(0, 0), 1.0f0)
+    end
+    lines!(p, p.circle; color = p.surfacecolor, alpha = p.surfacealpha, linewidth = p.surfaceboundary)
+    return p
+end
+
+function sphereplot(
+        M::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{1}}};
+        size = (1024, 1024), backgroundcolor = :white, show_axis = false, aspect = Makie.DataAspect(), kwargs...
+    )
+    fig = Figure(; backgroundcolor = backgroundcolor, size = size)
+    ax = Axis(fig[1, 1])
+    ax.aspect = aspect
+    if !show_axis
+        hidedecorations!(ax)
+        hidespines!(ax)
+    end
+    pl = sphereplot!(ax, M; kwargs...)
+    return Makie.FigureAxisPlot(fig, ax, pl)
 end
 
 function Makie.plot!(p::SpherePlot{<:Tuple{Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{2}}}}})
@@ -65,12 +104,39 @@ end
 # For `scatter(M, pts)`, `lines(M, pts)`, `scatterlines(M, pts)`
 # (and any other PointBased plot) work on a manifold via this overload.
 # We do not have to transform the points
+function Makie.convert_arguments(P::Makie.PointBased, ::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{1}}}, pts::V) where {V <: AbstractVector{<:AbstractVector}}
+    return Makie.convert_arguments(P, Point2f.(pts))
+end
+# we already have P3fs, just pass down
+function Makie.convert_arguments(P::Makie.PointBased, ::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{1}}}, pts::V) where {V <: AbstractVector{<:Point}}
+    return Makie.convert_arguments(P, pts)
+end
+# S2
 function Makie.convert_arguments(P::Makie.PointBased, ::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{2}}}, pts::V) where {V <: AbstractVector{<:AbstractVector}}
     return Makie.convert_arguments(P, Point3f.(pts))
 end
 # we already have P3fs, just pass down
 function Makie.convert_arguments(P::Makie.PointBased, ::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{2}}}, pts::V) where {V <: AbstractVector{<:Point}}
     return Makie.convert_arguments(P, pts)
+end
+
+# For arrows2d(M, pts, vecs) we want to combine the classical scatter with arrows,
+# where we assume that vecs[i] is in the tangent space of pts[1]
+# (a) From Manifolds.jl
+function Makie.convert_arguments(
+        ::Makie.ArrowLike, ::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{1}}}, pts::V, vecs::W
+    ) where {V <: AbstractVector{<:AbstractVector}, W <: AbstractVector{<:AbstractVector}}
+    #Not 100 % sure why the [1] is necessary, taken from conversions happening in arrows.jlr
+    return (
+        convert_arguments(Makie.PointBased(), Point2f.(pts))[1], convert_arguments(Makie.PointBased(), Vec2f.(vecs))[1],
+    )
+end
+# (b) already in Point2f
+function Makie.convert_arguments(::Makie.ArrowLike, ::Manifolds.Sphere{ℝ, Manifolds.TypeParameter{Tuple{1}}}, pts::V, vecs::W) where {V <: AbstractVector{<:Point}, W <: AbstractVector{<:Vec}}
+    #Not 100 % sure why the [1] is necessary, taken from conversions happening in arrows.jlr
+    return (
+        convert_arguments(Makie.PointBased(), pts)[1], convert_arguments(Makie.PointBased(), vecs)[1],
+    )
 end
 
 # For arrows3d(M, pts, vecs) we want to combine the classical scatter with arrows,
